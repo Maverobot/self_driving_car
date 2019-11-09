179f5fd3d122239d83360b5613e0999d7463bd4b
Maverobot <quzhengrobot@gmail.com>
2019-11-09 15:13:36 +0100

    fixed some empty checks

--- finding_lane/lanes.py
+++ finding_lane/lanes.py
@@ -35,8 +35,10 @@ def region_of_interest(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for x1, y1, x2, y2 in lines:
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+        for line in lines:
+            if len(line) == 4:
+                x1, y1, x2, y2 = line
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
     return line_image
 
 
@@ -52,10 +54,16 @@ def average_slope_intercept(image, lines):
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope, intercept))
-    left_fit_average = np.average(left_fit, 0)
-    right_fit_average = np.average(right_fit, 0)
-    left_line = make_coordinates(image, left_fit_average)
-    right_line = make_coordinates(image, right_fit_average)
+
+    left_line = []
+    right_line = []
+    if left_fit:
+        left_fit_average = np.average(left_fit, 0)
+        left_line = make_coordinates(image, left_fit_average)
+    if right_fit:
+        right_fit_average = np.average(right_fit, 0)
+        right_line = make_coordinates(image, right_fit_average)
+
     return np.array([left_line, right_line])
 
 
@@ -69,27 +77,33 @@ def make_coordinates(image, line_parameters):
 
 
 def main():
-    # Read image from file
-    image = cv2.imread("test_image.jpg")
-    lane_image = np.copy(image)
+    cap = cv2.VideoCapture("test2.mp4")
+    while cap.isOpened():
+        _, frame = cap.read()
 
-    # Get canny image
-    canny_image = canny(lane_image)
+        # Get canny image
+        canny_image = canny(frame)
 
-    # Masked image
-    masked_image = region_of_interest(canny_image)
+        # Masked image
+        masked_image = region_of_interest(canny_image)
 
-    lines = cv2.HoughLinesP(
-        masked_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5
-    )
+        lines = cv2.HoughLinesP(
+            masked_image,
+            2,
+            np.pi / 180,
+            100,
+            np.array([]),
+            minLineLength=40,
+            maxLineGap=5,
+        )
 
-    averaged_lines = average_slope_intercept(lane_image, lines)
+        averaged_lines = average_slope_intercept(frame, lines)
 
-    line_image = display_lines(lane_image, averaged_lines)
+        line_image = display_lines(frame, averaged_lines)
 
-    combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
-    cv2.imshow("result", combo_image)
-    cv2.waitKey(0)
+        combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 0)
+        cv2.imshow("result", combo_image)
+        cv2.waitKey(1)
 
 
 if __name__ == "__main__":
