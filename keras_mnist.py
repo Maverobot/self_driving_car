e6b20e87fef82daade228e8ce1b7e0fced46c186
Maverobot <quzhengrobot@gmail.com>
2019-11-12 23:13:14 +0100

    WIP keras mnist with fully connected layers

---
+++ keras_mnist.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import random
+
+mnist = keras.datasets.mnist
+Sequential = keras.Sequential
+Dense = keras.layers.Dense
+Adam = keras.optimizers.Adam
+to_categorical = keras.utils.to_categorical
+
+
+def check_dataset(X_train, y_train, X_test, y_test):
+    assert (
+        X_train.shape[0] == y_train.shape[0],
+        "The number of images is not equal to the number of labels.",
+    )
+    assert (
+        X_test.shape[0] == y_test.shape[0],
+        "The number of images is not equal to the number of labels.",
+    )
+    assert (X_train.shape[1:] == (28, 28), "The dimensions of the images are not 28x28")
+    assert (X_test.shape[1:] == (28, 28), "The dimensions of the images are not 28x28")
+
+
+def show_sample_images(X_train, y_train, num_samples=5):
+    num_of_samples = []
+    num_classes = len(set(y_train))
+    fig, axe = plt.subplots(nrows=num_classes, ncols=num_samples, figsize=(10, 10))
+    fig.tight_layout()
+    for i in range(num_classes):
+        x_selected = X_train[y_train == i]
+        for j in range(num_samples):
+            axe[i][j].imshow(
+                x_selected[random.randint(0, len(x_selected) - 1)],
+                cmap=plt.get_cmap("gray"),
+            )
+            axe[i][j].axis("off")
+            if j == 2:
+                axe[i][j].set_title(str(i))
+                num_of_samples.append(len(x_selected))
+
+    plt.figure(figsize=(12, 4))
+    plt.bar(range(0, num_classes), num_of_samples)
+    plt.title("Distribution of the training dataset")
+    plt.xlabel("Class number")
+    plt.ylabel("Number of images")
+
+
+def create_model(input_dim, num_classes):
+    model = Sequential()
+    model.add(Dense(units=10, input_dim=input_dim, activation="relu"))
+    model.add(Dense(units=10, activation="relu"))
+    model.add(Dense(units=num_classes, activation="softmax"))
+    model.compile(Adam(lr=0.01), loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+
+def main():
+    np.random.seed(0)
+
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    print(X_train.shape)
+    print(y_train.shape)
+    check_dataset(X_train, y_train, X_test, y_test)
+    show_sample_images(X_train, y_train)
+    num_classes = len(set(y_train))
+
+    # Use one-hot encoding
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
+    # Normalize pixel intensity.
+    # Consider the sigmoid function where e^255 would be 5.5E110 while e^1 is just 2.7.
+    # It would be much easier to train after the normalization.
+    X_train = X_train / 255
+    X_test = X_test / 255
+
+    # Flatten the datao
+    input_dim = X_train.shape[1] * X_train.shape[2]
+    X_train = X_train.reshape(X_train.shape[0], input_dim)
+    X_test = X_test.reshape(X_test.shape[0], input_dim)
+
+    model = create_model(input_dim, num_classes)
+    print(model.summary())
+    model.fit(
+        x=X_train,
+        y=y_train,
+        validation_split=0.1,
+        verbose=1,
+        epochs=10,
+        batch_size=200,
+        shuffle=True,
+    )
+
+    plt.show(block=True)
+    return True
+
+
+if __name__ == "__main__":
+    main()
